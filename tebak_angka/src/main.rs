f81c286ab18450621fbb33ea93b96aa2ad7c6309
zevtyardt <44270905+zevtyardt@users.noreply.github.com>
2023-03-16 05:33:25 +0700

    Update main.rs

--- tebak_angka/src/main.rs
+++ tebak_angka/src/main.rs
@@ -4,14 +4,14 @@ use std::cmp::Ordering;
 use rand::Rng;
 
 fn main() {
-    let angka = rand::thread_rng().gen_range(1..101);
+    let angka = rand::thread_rng().gen_range(1..10001);
     let mut percobaan = 0;
     
     println!("\nGAME SEDERHANA MENEBAK ANGKA DARI 1 SAMPAI 100\n");
 
     loop {
         let mut tebakan = String::new();
-        print!("tebakanmu (1 - 100): ");
+        print!("tebakanmu (1 - 10000): ");
         io::stdout().flush().expect("");
         io::stdin().read_line(&mut tebakan).expect("oops, ada error");
         let tebakan: u32 = match tebakan.trim().parse() {
