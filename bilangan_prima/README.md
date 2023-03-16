cd3e5a2edcd6cec8f53bb0e4079498a04ee6f803
zevtyardt <apklover76@gmail.com>
2023-03-16 08:04:06 +0700

    menambahkan folder bilangan_prima

---
+++ bilangan_prima/README.md
@@ -0,0 +1,10 @@
+#### output
+
+```
+$ ./main
+masukan angka: 7
+hasil: 7 adalah bilangan prima
+$ ./main
+masukan angka: 69
+hasil: 69 bukan bilangan prima
+```
---
+++ bilangan_prima/src/main.rs
@@ -0,0 +1,35 @@
+use std::io;
+use std::io::Write;
+
+fn is_prime(m: &u64) -> bool {
+    if *m as i64 == 1 {
+        return true;
+    }
+    for divisor in 2..*m {
+        if m % divisor == 0 {
+            return false;
+        }
+    }
+    return true;
+}
+
+fn main() {
+    loop {
+        let mut angka = String::new();
+        print!("masukan angka: ");
+        io::stdout().flush().expect("");
+        io::stdin().read_line(&mut angka).expect("opps, ada error");
+        let angka: u64 = match angka.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue
+        };
+        let prima = is_prime(&angka);
+        
+        if prima {
+            println!("hasil: {} adalah bilangan prima", angka);
+        } else {
+            println!("hasil: {} bukan bilangan prima", angka);
+        }
+        return;
+    }    
+}
