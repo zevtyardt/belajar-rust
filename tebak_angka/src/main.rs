7835dfb6e04cb9109a1d21ffafac7c362bd8c0c0
zevtyardt <apklover76@gmail.com>
2023-03-17 09:04:42 +0700

    fix typo

--- tebak_angka/src/main.rs
+++ tebak_angka/src/main.rs
@@ -1,22 +1,24 @@
+use rand::Rng;
+use std::cmp::Ordering;
 use std::io;
 use std::io::Write;
-use std::cmp::Ordering;
-use rand::Rng;
 
 fn main() {
     let angka = rand::thread_rng().gen_range(1..10001);
     let mut percobaan = 0;
-    
-    println!("\nGAME SEDERHANA MENEBAK ANGKA DARI 1 SAMPAI 100\n");
+
+    println!("\nGAME SEDERHANA MENEBAK ANGKA DARI 1 SAMPAI 10000\n");
 
     loop {
         let mut tebakan = String::new();
         print!("tebakanmu (1 - 10000): ");
         io::stdout().flush().expect("");
-        io::stdin().read_line(&mut tebakan).expect("oops, ada error");
+        io::stdin()
+            .read_line(&mut tebakan)
+            .expect("oops, ada error");
         let tebakan: u32 = match tebakan.trim().parse() {
             Ok(num) => num,
-            Err(_) => continue
+            Err(_) => continue,
         };
         percobaan += 1;
 
@@ -27,7 +29,7 @@ fn main() {
             Ordering::Greater => println!("terlalu besar"),
             Ordering::Equal => {
                 println!("kamu menang di percobaan ke-{}", percobaan);
-                break
+                break;
             }
         }
     }
